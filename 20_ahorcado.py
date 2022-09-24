178dd9fe1cab80ae7507a06a1a83f2cc94cf1b3f
Carlos Hernandez <21superbeast@gmail.com>
2022-09-24 00:19:29 -0500

    Elaboracion del menu principal, dificultad, listar palabras, limpiar pantalla

--- 20_ahorcado.py
+++ 20_ahorcado.py
@@ -1,3 +1,8 @@
+from ast import While
+import platform
+import os
+import random
+
 def hangman():
     oportunidad=[
                 """
@@ -76,11 +81,54 @@ def banner_inicio():
     return banner
 
 
+def lista_palabras():
+    lista=[]
+    with open("./files/data.txt", "r",encoding="utf-8") as f: 
+        for line in f:
+            lista.append(line)
+    return lista
+
+
+def limpiar_pantalla():
+    sistema = platform.system()
+    sistema="cls" if sistema=="Windows" else "clear"
+    os.system(sistema)
+
+
+def jugar(dificultad):
+    lista=lista_palabras()
+    indice=random.randint(0,len(lista)-1)
+    print("indice: "+str(indice)+" No.Palabras: "+str(len(lista)))
+
+
+
+
 def run():
-    banner=banner_inicio()
-    print(banner)
-    for item in hangman():
-        print(item)
+    banner=banner_inicio()    
+    salir=True
+    dificultad=0
+
+    while salir==True:
+        print(banner)
+        print("Selecciona una opcion del menu, presionando la tecla correspondiente y enter")
+        seleccion=input("\n1 - Jugar\n2 - Seleccionar dificultad\n3 - Revisar la lista de palabras\n0 - Salir\n")
+        #El siguiente switch solo funciona en python 3.10
+        #https://towardsdatascience.com/switch-case-statements-are-coming-to-python-d0caf7b2bfd3
+        match seleccion:
+            case "1": #jugar, pasar como argumento dificultad
+                jugar(dificultad)
+            case "2": #dificultad
+                limpiar_pantalla()
+                dificultad=int(input("Nivel de dificultad\n0 - Facil(Ayuda de una letra)\n1 - Intermedio(Ayuda de una vocal)\n2 - Dificil(Sin ayudas)\n"))
+                if(dificultad<0 or dificultad>2):
+                    dificultad=0                
+            case "3": #Lista de palabras
+                for palabra in lista_palabras():
+                    print(palabra)
+            case "0": #Salir
+                salir=False
+            case _:
+                print("Opcion no valida")
 
 
 if __name__ == '__main__':
